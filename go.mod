3ada139c13eb03c99d0ddce36e94e1b5114946ac
Marten Seemann <martenseemann@gmail.com>
2022-11-03 17:42:37 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/marten-seemann/testrepo2
+
+go 1.19
+
+require github.com/marten-seemann/testrepo v0.1.0
+
+require pgregory.net/rand v1.0.2 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+
+  "github.com/marten-seemann/testrepo"
+)
+
+func main() {
+	fmt.Println("Here's a random number:", testrepo.Rand())
+}
